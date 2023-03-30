3ce6610b2bd2329df025fc20b13a9e619b33b293
chetan kirange <chetan@shivamitech.com>
2023-03-30 05:41:38 +0530

    episode 02

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", {id:"parent" },[
     React.createElement("div", {id: "child" },[
-        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h1", {}, "Hello I am Chetan"),
         React.createElement("h2", {}, "I'm h2 tag")
     ]),
     React.createElement("div", {id: "child 2" },[
